c1f999909fb49422cc1824329c63be0e75e82c3b
twinkybells <162160608+twinkybells@users.noreply.github.com>
2024-04-21 02:48:51 +0800

    Add files via upload

---
+++ APPSDEV Twinky/Appsdev-System/Appsdev-System/Records.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appsdev_System
+{
+    public partial class Records : Form
+    {
+        public Records()
+        {
+            InitializeComponent();
+        }
+
+        private void Records_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
